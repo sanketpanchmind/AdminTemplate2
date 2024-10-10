6ea1c8122badede4da2014d09c6b3f0ece96b77e
Sanket <panchmind1@gmail.com>
2024-10-10 15:11:00 +0530

    Add project files.

---
+++ AdminTemplate2/Controllers/Dashboard1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminTemplate2.Controllers
+{
+    public class Dashboard1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AdminTemplate2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminTemplate2.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
